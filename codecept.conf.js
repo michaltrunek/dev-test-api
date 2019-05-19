6c413c9d3baacaec49d05978c708fe1859a07607
Michal Trunek <michal.trunek@tipico.com>
2019-05-19 21:50:31 +0200

    run scripts updated, not needed dep removed, codecept conf file clearen

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,48 +1,25 @@
-let reporterVerbose = process.env.reporterVerbose;
-let reporterSteps = process.env.reporterSteps;
-let reporterDebug = process.env.reporterDebug;
-
-/**
- * Setting Env Variables default
- */
-if (reporterVerbose === undefined || reporterVerbose === 'false') reporterVerbose = false;
-else reporterVerbose = true;
-if (reporterSteps === undefined || reporterSteps === 'true') reporterSteps = true;
-else reporterSteps = false;
-if (reporterDebug === undefined || reporterDebug === 'false') reporterDebug = false;
-else reporterDebug = true;
-
-if (process.env.retries === undefined) process.env.retries = 0;
-if (process.env.NODE_ICU_DATA !== "node_modules/full-icu") process.env.NODE_ICU_DATA = "node_modules/full-icu";
-
 exports.config = {
     tests: './tests/*Test.js',
     output: './output',
 
     helpers: {
         REST: {
-            timeout: 20000,
+            timeout: 30000,
             endpoint: "http://localhost:8080/",
         }
     },
     include: {
         I: './steps_file.js',
-        devowelizerHelper: './helpers/devowelizerHelper.js'
+        methodHelper: './helpers/methodHelper.js',
+        testData: './helpers/testData.js'
     },
     bootstrap: false,
     mocha: {
         reporterOptions: {
             reportDir: './output',
             enableCharts: true,
-            reportTitle: 'Desktop Reporter',
-            mochaFile: "output/result.xml",
-            rootSuiteTitle: 'Desktop Tests'
+            reportTitle: 'Devowelizer API test results'
         }
     },
-    name: 'devowelizer',
-    plugins: {
-        allure: {
-            enabled: false
-        }
-    }
+    name: 'devowelizer api testing'
 };
